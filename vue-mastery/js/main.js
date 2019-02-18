5ec9bfc10d65fa8d2af3b9b7a07b75fd38344cc1
Jonathan <jonathan.da.s.lima@accenture.com>
2019-02-18 17:47:23 -0300

    class 5

--- vue-mastery/js/main.js
+++ vue-mastery/js/main.js
@@ -6,9 +6,29 @@ var app = new Vue({
 		image: './img/vmSocks-green-onWhite.jpg',
 		description: 'Socks',
 		link: 'http://www.jonathanslima.github.io',
-		stock: 0,
-		details: [
-			'80% cotton', '20% poliester', 'Gender-neutral'
-		]
+		inStock: true,
+		details: ['80% cotton', '20% poliester', 'Gender-neutral'],
+		variants: [
+			{
+				variantId: 2234,
+				variantColor: 'green',
+				variantImage: './img/vmSocks-green-onWhite.jpg'
+			},
+			{
+				variantId: 2235,
+				variantColor: 'blue',
+				variantImage: './img/vmSocks-blue-onWhite.jpg'
+			}
+		],
+		sizes: ['P', 'M', 'G'],
+		cart: 0
+	},
+	methods: {
+		addToCart: function(){
+			this.cart += 1;
+		},
+		updateProduct: function(variantImg){
+			this.image = variantImg;
+		}
 	}
 });
