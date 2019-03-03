a6f7c694dc4593eeed44a6a5d044dbe2187a0f17
jonathanslima <jonathan633.jl@gmail.com>
2019-03-03 05:51:22 -0300

    finalizando aula 4

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,60 +1,65 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    <h1 class="centralizado">{{ title }}</h1>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="image in images">
+        <!-- Add component passing data to it -->
+        <meu-painel :titulo="image.titulo">
+          <img class="imagem-responsiva" :src="image.url" :alt="image.titulo">
+        </meu-painel>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+// Import component  
+import Painel from './components/shared/painel/Painel.vue'
+
+// declare painel to use at page
 export default {
-  name: 'app',
+  components: {
+    "meu-painel": Painel
+  },
   data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    return{
+      title: 'Alurapic',
+      images: []
     }
+  },
+  created (){
+    // call a promise
+    let promise = this.$http.get('http://localhost:3000/v1/fotos');
+
+    // resolve promise and then get data
+    promise
+      .then(res => res.json())
+      .then(images => this.images = images, err => console.log(err))
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body{
+    font-family: Helvetica;
+    width: 96%;
+    margin: 0 auto;
+  }
 
-h1, h2 {
-  font-weight: normal;
-}
+  .centralizado{
+    text-align: center;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  .lista-fotos{
+    list-style: none;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  .lista-fotos-item{
+    display: inline-block;
+  }
 
-a {
-  color: #42b983;
-}
+  .imagem-responsiva {
+    width: 100%;
+  }
 </style>
--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ViewResource from 'vue-resource'
+
+Vue.use(ViewResource)
 
 new Vue({
   el: '#app',
